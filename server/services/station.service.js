88203bc99f2f07f91a19012609905d75161c7781
Lim Howe <limhowe1214@gmail.com>
2018-03-30 05:21:21 +1000

    ADD: ability to get hourly snapshots

--- server/services/station.service.js
+++ server/services/station.service.js
@@ -1,8 +1,8 @@
-import Station from '../models/station'; //eslint-disable-line
+import Station from '../models/station';
+import Entry from '../models/entry';
 import * as DateManager from '../util/dateManager';
 
-
-async function getHourlySnapshots(from, to, kioskId) { //eslint-disable-line
+async function getHourlySnapshots(from, to, kioskId) {
   const rangeDates = DateManager.getRange(from, to);
   const stations = await Station.find({
     kioskId,
@@ -19,6 +19,7 @@ async function getHourlySnapshots(from, to, kioskId) { //eslint-disable-line
   const result = stations.map((item) => {
     const stationData = item.toObject();
     const { _entry, ...station } = stationData;
+    if (_entry.weather._id) delete _entry.weather._id;
     return {
       at: _entry.at,
       station,
@@ -58,7 +59,20 @@ async function getDailySnapShots(from, to, kioskId) {
     .group(ruleGroup)
     .exec();
 
-  return stations;
+  if (!stations || !stations.map || !stations.length) return null;
+  const promises = stations.map((item) => {
+    return Entry.findOne({ at: item.at }).select('-_id -stations -__v -weather._id').exec();
+  });
+  const entries = await Promise.all(promises);
+  const result = stations.map((station, i) => {
+    const _entry = entries[i].toObject();
+    return {
+      at: _entry.at,
+      station,
+      weather: _entry.weather,
+    };
+  });
+  return result;
 }
 
 export async function getSnapShots(from, to, kioskId, frequency = 'hourly') {
